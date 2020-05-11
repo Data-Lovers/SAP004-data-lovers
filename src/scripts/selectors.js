f0684932a0caeca404b8701af2f815d3287ebeda
coderanac <coder.anac@gmail.com>
2020-05-11 16:18:18 -0300

    :heavy_plus_sign: add selectors

--- src/scripts/selectors.js
+++ src/scripts/selectors.js
@@ -11,5 +11,10 @@ const searchFilter = document.querySelector('#search-filter');
 const searchIcon = document.querySelector('#search-icon');
 const searchNave = document.querySelector('#search-nave');
 
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuNav = document.querySelector('#menu-nav');
 
-export { allCharacters, cardsBox, gendersFilter, searchIcon, searchFilter, searchNave, search, speciesFilter, sortByNameUp, sortByNameDown, statusFilter };
+
+
+export { allCharacters, cardsBox, gendersFilter, menuOpen, menuClose, menuNav, searchIcon, searchFilter, searchNave, search, speciesFilter, sortByNameUp, sortByNameDown, statusFilter };
