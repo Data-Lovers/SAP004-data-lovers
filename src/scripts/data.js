20914955e6f32852f0594f405b2b87cacd08c0c6
Sofia <sofiasimas1992@gmail.com>
2020-05-17 15:04:29 -0300

    add status rick

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -30,6 +30,28 @@ export function sumRicksMorty (data) {
     }
     return accumulator;
   }
-  
+
   return data.reduce(groupRicksMorys , initialValue)
+}
+
+export function rickByStatus (data) {
+  const initialValue = { 
+    alive:  0,
+    dead:0,
+    unknown:0
+  }
+  function groupRicks (accumulator, current){
+    if(current.name.includes('Rick')){
+      if (current.status === 'Alive') {
+        accumulator.alive += 1;
+      } else if(current.status === 'Dead'){
+        accumulator.dead += 1; 
+      } else if(current.status === 'unknown'){
+        accumulator.unknown += 1;
+      }
+
+    }
+    return accumulator;
+  }
+  return data.reduce(groupRicks, initialValue)
 }
